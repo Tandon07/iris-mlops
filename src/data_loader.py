38efe716c6b81c6d9c1b5314347eff0180fe36b6
tando <tandonsaurabh07@gmail.com>
2025-08-05 22:21:53 +0530

    Track iris dataset with DVC

---
+++ src/data_loader.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from sklearn.datasets import load_iris
+
+def load_iris_data():
+    iris = load_iris(as_frame=True)
+    df = iris.frame
+    df.columns = [col.lower().replace(" ", "_") for col in df.columns]
+    return df
+
+if __name__ == "__main__":
+    df = load_iris_data()
+    df.to_csv("data/Iris.csv", index=False)
